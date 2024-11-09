8bedc56100da1d2c11f34eecc10e7c772827e2c1
PolinaKosstenko <Poli.na-Kostenko@yandex.ru>
2024-11-09 15:41:04 +1000

    add visit

--- code.py
+++ code.py
@@ -1,1 +1,14 @@
 import matplotlib
+def visit(self, url: str) -> None:
+        """
+        Returns - None
+        Input - str
+        ----------
+        - Adds the current url to the DLL
+        - sets both the next and previous values
+        """
+        url_node = DLL(url)
+        self.curr.nxt = url_node
+        url_node.prev = self.curr
+        
+        self.curr = url_node
